032f8ef6f1f4fa8e9e62dc0036be0e6d58a4d039
Justin Zimmerman <01001010tz@gmail.com>
2018-11-06 10:35:09 -0500

    added javascript

---
+++ hellothereworld.js
@@ -0,0 +1,17 @@
+function sayHC() {
+  console.log("Hello, World!");
+}
+
+function sayHPU() {
+  alert("Hello, World!");
+}
+
+function sayHB() {
+  var p=document.getElementById('hellobrowser');
+  p.innerHTML="Hello, World!";
+}
+
+function sayHS() {
+  var strangername=prompt('What is your name, stranger?');
+  alert("Hello, " + strangername);
+}
